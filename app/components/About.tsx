3347ffd131162f076ef06e9d9a751fa62d12dff7
AdamSz98 <szekrenyes.adam98@gmail.com>
2023-07-28 13:41:47 +0200

    Add picture and basic layout to about

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,9 +1,26 @@
-import styles from './About.module.css';
+import styles from "./About.module.css";
+import Image from "next/image";
 
 const About = () => {
   return (
     <div className={styles.about}>
-      About
+      <div className={styles.imgContainer}>
+        <Image
+          className={styles.img} 
+          src="/images/me.jpg"
+          alt=""
+          width={5482}
+          height={3647}
+        />
+      </div>
+      <div className={styles.textContainer}>
+        <h1 className={styles.secondary}>About</h1>
+        <h1 className={styles.primary}></h1>
+        <p className={styles.text}>
+
+        </p>
+      </div>
+
     </div>
   )
 }
