5aa60be61b688402c6cfa16aef2b61fe6fe26ff8
AdamSz98 <szekrenyes.adam98@gmail.com>
2023-07-28 14:43:50 +0200

    Add introduction text and style to about component

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -13,14 +13,21 @@ const About = () => {
           height={3647}
         />
       </div>
-      <div className={styles.textContainer}>
-        <h1 className={styles.secondary}>About</h1>
-        <h1 className={styles.primary}></h1>
-        <p className={styles.text}>
-
-        </p>
+      <div className={styles.sidebar}>
+        <div className={styles.textContainer}>
+          <h1 className={styles.secondary}>About</h1>
+          <h1 className={styles.primary}>Full-Stack Developer</h1>
+          <p className={styles.text}>
+            Hello there! My name is <span className={styles.highlight}>Adam Szekrényes</span>, 
+            I am a junior full-stack developer, who has recently 
+            graduated from Green Fox Academy. 
+            I have a huge passion for programming and my skills 
+            include developing websites and web applications. 
+            My other passions and hobbies are making music, 
+            videogame development, and of course, playing videogames. 
+          </p>
+        </div>
       </div>
-
     </div>
   )
 }
