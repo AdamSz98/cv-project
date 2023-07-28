9bd93b3a69f7234e9eb59b2ebbe5efc18069ea4e
AdamSz98 <szekrenyes.adam98@gmail.com>
2023-07-28 18:02:30 +0200

    Add skill level display

--- app/components/skills/Card.tsx
+++ app/components/skills/Card.tsx
@@ -3,18 +3,31 @@ import styles from './Card.module.css';
 interface CardProps {
   label: string;
   icon: any;
-  style: string;
+  color: string;
+  next?: boolean;
+  progress: number;
 }
 
 const Card: React.FC<CardProps> = ({
   label,
   icon: Icon,
-  style
+  color,
+  next,
+  progress
 }) => {
   return (
     <div className={styles.card}>
-      <Icon className={styles[style]}/>
+      <Icon className={next ? styles.next : styles.icon} style={{color: color}}/>
       <h2 className={styles.label}>{label}</h2>
+      <div className={styles.container}>
+        <div 
+          className={styles.progress} 
+          style={
+            next? {background: '#222021', width: `${progress * 10}%`, margin: '0 0 0 -1px'} : 
+            {background: color, width: `${progress * 10}%`}
+          }
+        />
+      </div>
     </div>
   )
 }
--- app/components/skills/Skills.tsx
+++ app/components/skills/Skills.tsx
@@ -1,45 +1,81 @@
+'use client';
+
 import Card from './Card';
 import styles from './Skills.module.css';
 import { 
   SiJavascript, SiTypescript, SiCsharp, 
   SiNextdotjs, SiAngular, SiDotnet
 } from 'react-icons/si';
-
+import { useState } from 'react';
 
 const Skills = () => {
+  const [skills, setSkills] = useState([0, 0, 0, 0, 0, 0,]);
+
+  const showSkills = () => {
+    setSkills([8, 6, 5, 7, 4, 5]);
+  }
+
+  const deleteSkills = () => {
+    setSkills([0, 0, 0, 0, 0, 0]);
+  }
+  
+  const checkScroll = () => {
+    if(window.scrollY > 1500) {
+      if(window.scrollY > 2100) deleteSkills()
+      else showSkills();
+    } else {
+      deleteSkills();
+    }
+  }
+  
+  window.addEventListener('scroll', checkScroll);
+
   return (
-    <div className={styles.main}>
+    <div 
+      className={styles.main}
+    >
       <h1 className={styles.title}>Skills</h1>
+      <h2 className={styles.subtitle}>Programming Languages</h2>
       <div className={styles.container}>
         <Card 
           icon={SiJavascript} 
           label="JavaScript"
-          style="js"
+          color="#f0dc4e"
+          progress={skills[0]}
         />
         <Card 
           icon={SiTypescript} 
           label="TypeScript"
-          style="ts"
+          color="#2f74c0"
+          progress={skills[1]}
         />
         <Card 
           icon={SiCsharp} 
           label="C#"
-          style="cs"
+          color="#8864a4"
+          progress={skills[2]}
         />
+      </div>
+      <h2 className={styles.subtitle}>Frameworks</h2>
+      <div className={styles.container}>
         <Card 
           icon={SiNextdotjs} 
           label="Next.js"
-          style="nx"
+          color="#000"
+          next={true}
+          progress={skills[3]}
         />
         <Card 
           icon={SiAngular} 
           label="Angular"
-          style="an"
+          color="#de002d"
+          progress={skills[4]}
         />
         <Card 
           icon={SiDotnet} 
           label="ASP.NET Core"
-          style="dn"
+          color="#8864a4"
+          progress={skills[5]}
         />
       </div>
     </div>
