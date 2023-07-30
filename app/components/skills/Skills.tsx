6e91750350e62eb1a4b3b1124002751ef9213155
AdamSz98 <szekrenyes.adam98@gmail.com>
2023-07-30 20:31:16 +0200

    Fix styling for skills

--- app/components/skills/Skills.tsx
+++ app/components/skills/Skills.tsx
@@ -13,42 +13,46 @@ const Skills = () => {
       id="skillspage"
     >
       <h1 className={styles.title}>Skills</h1>
-      <h2 className={styles.subtitle}>Programming Languages</h2>
-      <div className={styles.container}>
-        <Card 
-          icon={SiJavascript} 
-          label="JavaScript"
-          color="#f0dc4e"
-        />
-        <Card 
-          icon={SiTypescript} 
-          label="TypeScript"
-          color="#2f74c0"
-        />
-        <Card 
-          icon={SiCsharp} 
-          label="C#"
-          color="#8864a4"
-        />
-      </div>
-      <h2 className={styles.subtitle}>Frameworks</h2>
-      <div className={styles.container}>
-        <Card 
-          icon={SiNextdotjs} 
-          label="Next.js"
-          color="#000"
-          next={true}
-        />
-        <Card 
-          icon={SiAngular} 
-          label="Angular"
-          color="#de002d"
-        />
-        <Card 
-          icon={SiDotnet} 
-          label="ASP.NET Core"
-          color="#8864a4"
-        />
+      <div className={styles.contentFrame}>
+        <div className={styles.content}>
+          <h2 className={styles.subtitle}>Programming Languages</h2>
+          <div className={styles.container}>
+            <Card 
+              icon={SiJavascript} 
+              label="JavaScript"
+              color="#f0dc4e"
+            />
+            <Card 
+              icon={SiTypescript} 
+              label="TypeScript"
+              color="#2f74c0"
+            />
+            <Card 
+              icon={SiCsharp} 
+              label="C#"
+              color="#8864a4"
+            />
+          </div>
+          <h2 className={styles.subtitle}>Frameworks</h2>
+          <div className={styles.container}>
+            <Card 
+              icon={SiNextdotjs} 
+              label="Next.js"
+              color="#000"
+              next={true}
+            />
+            <Card 
+              icon={SiAngular} 
+              label="Angular"
+              color="#de002d"
+            />
+            <Card 
+              icon={SiDotnet} 
+              label="ASP.NET Core"
+              color="#8864a4"
+            />
+          </div>
+        </div>
       </div>
     </div>
   )
