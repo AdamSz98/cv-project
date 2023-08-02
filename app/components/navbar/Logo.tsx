0abc5c0886728fdf1b878be7c74a2cc6a211375b
AdamSz98 <szekrenyes.adam98@gmail.com>
2023-08-02 17:52:45 +0200

    Logo changes according to window width

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -1,4 +1,6 @@
+'use client'
 import styles from './Logo.module.css';
+import { useState, useEffect } from 'react';
 
 interface LogoProps {
   onClick: any;
@@ -7,6 +9,17 @@ interface LogoProps {
 const Logo: React.FC<LogoProps> = ({
   onClick
 }) => {
+  const [name, setName] = useState('Adam Szekrényes');
+
+  useEffect(() => {
+    changeLogo()
+    window.addEventListener('resize', changeLogo)
+  })
+
+  const changeLogo = () => {
+    if(window.innerWidth > 600) setName('Adam Szekrényes');
+    else setName('adam');
+  }
 
   const toTheTop = () => {
     onClick()
@@ -23,7 +36,7 @@ const Logo: React.FC<LogoProps> = ({
       onClick={toTheTop}
       href="#home"
     >
-      Adam Szekrényes
+      {name}
     </a>
   )
 }
