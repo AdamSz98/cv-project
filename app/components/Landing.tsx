9fc9783b5f15c798f9004684169a5378722ada74
AdamSz98 <szekrenyes.adam98@gmail.com>
2023-08-02 15:29:46 +0200

    Remove landing page img-swap functionality

--- app/components/Landing.tsx
+++ app/components/Landing.tsx
@@ -5,23 +5,14 @@ import Image from 'next/image';
 import { useState, useEffect } from 'react';
 
 const Landing = () => {
-  const initialNum = Math.floor(Math.random() * 6 + 1);
-  const [cityNum, setCityNum] = useState(initialNum);
+  const cityNum = Math.floor(Math.random() * 6 + 1);
   const [loaded, setLoaded] = useState(false);
-
-  const changeCity = () => {
-    if (window.scrollY < window.innerHeight) return;
-    let newNum = Math.floor(Math.random() * 6 + 1);
-    if(newNum === cityNum) newNum = Math.floor(Math.random() * 6 + 1);
-    setCityNum(newNum);
-  }
 
   useEffect(() => {
     setLoaded(true);
   }, [])
 
-  if(loaded) window.addEventListener('scroll', changeCity);
-
+  if (!loaded) return;
   return (
     <section className={styles.landing}>
         <Image
