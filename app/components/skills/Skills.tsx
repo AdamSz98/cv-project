48219c3b250ad4e527e3616969a00a219bebb93e
AdamSz98 <szekrenyes.adam98@gmail.com>
2023-08-02 13:07:56 +0200

    Redesign of skills section

--- app/components/skills/Skills.tsx
+++ app/components/skills/Skills.tsx
@@ -1,8 +1,8 @@
 import Card from './Card';
 import styles from './Skills.module.css';
 import { 
-  SiJavascript, SiTypescript, SiCsharp, 
-  SiNextdotjs, SiAngular, SiDotnet
+  SiHtml5, SiCss3, SiJavascript, SiTypescript, 
+  SiCsharp, SiReact, SiNextdotjs, SiDotnet
 } from 'react-icons/si';
 
 
@@ -13,47 +13,49 @@ const Skills = () => {
       id="skillspage"
     >
       <h1 className={styles.title}>Skills</h1>
-      <div className={styles.contentFrame}>
+      <div className={styles.container}>
         <div className={styles.content}>
-          <h2 className={styles.subtitle}>Programming Languages</h2>
-          <div className={styles.container}>
-            <Card 
-              icon={SiJavascript} 
-              label="JavaScript"
-              color="#f0dc4e"
-            />
-            <Card 
-              icon={SiTypescript} 
-              label="TypeScript"
-              color="#2f74c0"
-            />
-            <Card 
-              icon={SiCsharp} 
-              label="C#"
-              color="#8864a4"
-            />
-          </div>
-        </div>
-        <div className={styles.content}>
-          <h2 className={styles.subtitle}>Frameworks</h2>
-          <div className={styles.container}>
-            <Card 
-              icon={SiNextdotjs} 
-              label="Next.js"
-              color="#000"
-              next={true}
-            />
-            <Card 
-              icon={SiAngular} 
-              label="Angular"
-              color="#de002d"
-            />
-            <Card 
-              icon={SiDotnet} 
-              label="ASP.NET Core"
-              color="#8864a4"
-            />
-          </div>
+        <Card 
+            icon={SiHtml5} 
+            label="HTML"
+            color="#e84c24"
+          />
+          <Card 
+            icon={SiCss3} 
+            label="CSS"
+            color="#289cfc"
+          />
+          <Card 
+            icon={SiJavascript} 
+            label="JavaScript"
+            color="#f0dc4e"
+          />
+          <Card 
+            icon={SiTypescript} 
+            label="TypeScript"
+            color="#2f74c0"
+          />
+          <Card 
+            icon={SiCsharp} 
+            label="C#"
+            color="#8864a4"
+          />
+          <Card 
+            icon={SiReact} 
+            label="React"
+            color="#61dbfb"
+          />
+          <Card 
+            icon={SiNextdotjs} 
+            label="Next.js"
+            color="#000"
+            next={true}
+          />
+          <Card 
+            icon={SiDotnet} 
+            label="ASP.NET Core"
+            color="#8864a4"
+          />
         </div>
       </div>
     </div>
